763a27876b07d4b79af6b89d6203b28f33528b47
Sir Luck <63381079+Luckybatwing@users.noreply.github.com>
2022-05-17 16:36:15 -0400

    Add player 2

--- Player.gd
+++ Player.gd
@@ -4,24 +4,23 @@ export var speed = 400  # How fast the player will move (pixels/sec).
 var screen_size: Vector2  # Size of the game window.
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var velocity = Vector2.ZERO
+func read_input():
+	assert(false, "Method not overridden")
 
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
+
+func _process(delta: float):
+	var velocity = read_input()
 
 	velocity *= speed
 
 	position += velocity * delta
-	position.y = clamp(position.y, 0, screen_size.y - $Paddle.polygon[2].y)
+	position.y = clamp(
+		position.y, $Paddle.polygon[2].y / 2, screen_size.y - ($Paddle.polygon[2].y / 2)
+	)
 
 
 func _on_Player_area_entered(area: Area2D):
---
+++ PlayerTwo.gd
@@ -0,0 +1,12 @@
+extends "res://Player.gd"
+
+
+func read_input() -> Vector2:
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_up_p2"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down_p2"):
+		velocity.y += 1
+
+	return velocity
+
